9163c06424fa75f827aec7f292eb2d57b904bdd0
nishmal <nishmalk94@gmail.com>
2023-11-06 14:15:46 +0530

    data update

--- task.py
+++ task.py
@@ -1,47 +1,55 @@
-import json
-
-# Read your data from a file (update the path to your data file)
-data_file_path = '/home/user/Documents/csv-new/sample.txt'  # Update the path to your data file
-
-# Initialize a dictionary to store the product conditions and their counts for both Description and Short Description
-product_conditions = {
-    'New': {'Description': 0, 'Short Description': 0, 'Total': 0},
-    'Refurbished': {'Description': 0, 'Short Description': 0, 'Total': 0},
-    'Renew': {'Description': 0, 'Short Description': 0, 'Total': 0},
-    'Remanufactured': {'Description': 0, 'Short Description': 0, 'Total': 0},
-    'Recertified': {'Description': 0, 'Short Description': 0, 'Total': 0}
-}
-
-# Function to check if a condition word exists in a given string
+import csv
+import re
 def check_condition(string, condition):
-    return condition.lower() in string.lower()
-
-# Read the data file and process each line
+    if condition == "Renew":
+        return "Renew" in string
+    else:
+        regex = re.escape(condition)
+        return bool(re.search(regex, string, re.IGNORECASE))
+def update_data(product_conditions, condition, description, short_description):
+    product_conditions[condition]['Description'].append(description)
+    product_conditions[condition]['Short Description'].append(short_description)
+    product_conditions[condition]['Total'].append(description + " | " + short_description)
+data_file_path = '/home/user/Documents/csv-new/sample.txt'  # Update the path to your data file
+product_conditions = {
+    'New': {'Description': [], 'Short Description': [], 'Total': []},
+    'Refurbished': {'Description': [], 'Short Description': [], 'Total': []},
+    'Renew': {'Description': [], 'Short Description': [], 'Total': []},
+    'Remanufactured': {'Description': [], 'Short Description': [], 'Total': []},
+    'Recertified': {'Description': [], 'Short Description': [], 'Total': []}
+}
+# Read the data from the file and update condition data
 with open(data_file_path, 'r') as data_file:
     for line in data_file:
         description, short_description = line.strip().split('|~|')
-
-        # Check if any of the condition words exist in Description and Short Description
         for condition in product_conditions:
-            if condition != 'Renew':
-                if check_condition(description, condition):
-                    product_conditions[condition]['Description'] += 1
-                    product_conditions[condition]['Total'] += 1
-                if check_condition(short_description, condition):
-                    product_conditions[condition]['Short Description'] += 1
-                    product_conditions[condition]['Total'] += 1
-            elif condition == 'Renew' and 'Renew' in description:
-                product_conditions[condition]['Description'] += 1
-                product_conditions[condition]['Total'] += 1
-            elif condition == 'Renew' and 'Renew' in short_description:
-                product_conditions[condition]['Short Description'] += 1
-                product_conditions[condition]['Total'] += 1
+            if check_condition(description, condition) or check_condition(short_description, condition):
+                update_data(product_conditions, condition, description, short_description)
+# Add "New" condition with no data
+product_conditions['New'] = {'Description': [], 'Short Description': [], 'Total': []}
+# Write the condition data to a CSV file
+csv_file_path = 'condition_data.csv'
+with open(csv_file_path, 'w', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    # Write the header row
+    csv_writer.writerow(['Condition', 'Description', 'Short Description', 'Total'])
+    for condition, data in product_conditions.items():
+        description_data = '\n'.join(data['Description'])
+        short_description_data = '\n'.join(data['Short Description'])
+        total_data = '\n'.join(data['Total'])
+        csv_writer.writerow([condition, description_data, short_description_data, total_data])
+print("Condition data including 'New' added and migrated to 'condition_data.csv'.")
 
-# Generate a JSON report
-report = json.dumps(product_conditions, indent=4)
+React
 
-# Save the report to a JSON file
-with open('product_condition_report.json', 'w') as report_file:
-    report_file.write(report)
+Reply
 
-print("Product condition report generated and saved to 'product_condition_report.json'.")
+
+
+
+
+
+
+
+
+
